df69e670baa7da19e44ab589a376df055259f73b
GPS_Gui_S <guilherme.perez200@gmail.com>
2026-03-11 19:37:59 -0300

    Adição dos métodos a versão base
    Adição dos métodos adicionarVarios, contarOcorrencias e ultimoIndiceDe

---
+++ Trabalho1-AED-atualiza-o-yuji-1.1/Trabalho1-AED-atualiza-o-yuji-1.1/src/aplicacao/Main.java
@@ -0,0 +1,38 @@
+import entidades.ListaDinamica;
+import entidades.ListaSimples;
+
+static void main() {
+
+    ListaSimples listaSimples = new ListaSimples(5);
+    listaSimples.estaCheio();
+    listaSimples.adicionarElemento("Arroz");
+    listaSimples.adicionarElemento("Feijão");
+    listaSimples.adicionarElemento("Sabão em Pó");
+    listaSimples.adicionarElemento("Papel Higiênico");
+    listaSimples.adicionarElemento("Óleo");
+    listaSimples.exibirElementos();
+    System.out.println(listaSimples.contar());
+    listaSimples.obter(5);
+    System.out.println(listaSimples.obter(2));
+    listaSimples.removerElemento("Batata");
+    listaSimples.buscarElemento("Óleo");
+    listaSimples.removerElemento("Óleo");
+    listaSimples.buscarElemento("Óleo");
+    listaSimples.buscarElemento("Arroz");
+    listaSimples.exibirElementos();
+    listaSimples.removerPorIndice(2);
+    listaSimples.exibirElementos();
+    System.out.println("----------------------------------------------------------");
+    ListaDinamica listaDinamica = new ListaDinamica();
+    listaDinamica.adicionarElemento("Arroz");
+    listaDinamica.adicionarElemento("Feijão");
+    listaDinamica.adicionarElemento("Batata");
+    listaDinamica.adicionarElemento("Alho");
+    listaDinamica.adicionarElemento("Cebola");
+    listaDinamica.removerElemento("Banana");
+    listaDinamica.exibir();
+    listaDinamica.contar();
+    System.out.println(listaDinamica.obter(2));
+    listaDinamica.removerPorIndice(2);
+    listaDinamica.exibir();
+}
---
+++ Trabalho1-AED-atualiza-o-yuji-1.1/Trabalho1-AED-atualiza-o-yuji-1.1/src/entidades/ListaSimples.java
@@ -0,0 +1,173 @@
+package entidades;
+
+public class ListaSimples implements ListaOperacoes {
+    public String[] lista;
+
+    public ListaSimples(int tamanho) {
+        this.lista = new String[tamanho];
+        System.out.println("Operação realizada com sucesso! Existem " + tamanho + " posições disponíveis.");
+    }
+    public void adicionarElemento(String elemento) {
+        if (!this.estaCheio()) {
+            lista[encontrarPosicao()] = elemento;
+        }
+    }
+    public boolean estaCheio() {
+        for (int i = 0; i < lista.length; i++) {
+            if (this.lista[i] == null) {
+                return false;
+            }
+        }
+        System.out.println("Não há espaço disponível na lista.");
+        return true;
+    }
+    private int encontrarPosicao() {
+        int i;
+        for (i = 0; i < lista.length; i++) {
+            if (this.lista[i] == null) {
+                break;
+            }
+        }
+        return i;
+    }
+    public void exibirElementos() {
+        for (int i = 0; i < lista.length; i++) {
+            System.out.println("Lista [" + i + "] = " + this.lista[i]);
+        }
+    }
+    public void removerElemento(String elemento) {
+        boolean removido = false;
+        if (!estaVazio()) {
+            for (int i = 0; i < lista.length; i++) {
+                if (this.lista[i] != null && this.lista[i].equals(elemento)) {
+                    this.lista[i] = null;
+                    removido = true;
+                }
+            }
+        }
+        if (removido) {
+            System.out.println("O elemento " + elemento + " foi removido com sucesso!");
+        } else {
+            System.out.println("O elemento " + elemento + " não existe na lista.");
+        }
+    }
+    public boolean estaVazio() {
+        for (int i = 0; i < lista.length; i++) {
+            if (this.lista[i] != null) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public void buscarElemento(String elemento) {
+        boolean encontrado = false;
+        if (!estaVazio()) {
+            for (int i = 0; i < lista.length; i++) {
+                if (this.lista[i] != null && this.lista[i].equals(elemento)) {
+                    encontrado = true;
+                }
+            }
+        }
+        if (encontrado) {
+            System.out.println("O elemento " + elemento + " existe na lista.");
+        } else {
+            System.out.println("O elemento " + elemento + " não existe na lista.");
+        }
+    }
+
+    //Júlio
+    @Override
+    public int removerTodas(String elemento) {
+        return 0;
+    }
+
+    //Yuji
+    @Override
+    public int contar() {
+        int quantidade = 0;
+        for (int i = 0; i < lista.length; i++) {
+            quantidade++;
+        }
+        return quantidade;
+    }
+
+    //Guilherme
+    @Override
+    public int adicionarVarios(String[] elementos) {
+        return 0;
+    }
+
+    //Yuji
+    @Override
+    public String obter(int indice) {
+        if (indice < 0 || indice >= lista.length) {
+            System.out.println("Índice inválido");
+            return null;
+        }
+        return lista[indice];
+    }
+
+    //Eduardo
+    @Override
+    public boolean inserir(int indice, String elemento) {
+        return false;
+    }
+
+    //Yuji
+    @Override
+    public String removerPorIndice(int indice) {
+        String removido = "";
+        if (indice < 0 || indice >= lista.length) {
+            return "Índice inválido.";
+        } else {
+            removido = lista[indice]; //guarda o elemento a ser removido
+            for (int i = indice; i < lista.length - 1; i++) { //percorre a lista até o último elemento
+                lista[i] = lista[i + 1]; //desloca a lista à direita
+            }
+            lista[lista.length - 1] = null; //remove o último elemento
+
+            return removido; //retorna o elemento removido
+        }
+    }
+    //Eduardo
+    @Override
+    public void limpar() {
+
+    }
+
+    //Guilherme
+    @Override
+    public int ultimoIndiceDe(String elemento) {
+        int ultimoIndice = -1;
+        String comparador = elemento.trim().toLowerCase();
+        if (contarOcorrencias(elemento) == 0) {
+            System.out.println("Não existe "+elemento+" na lista");
+            return ultimoIndice;
+        } else {
+            for (int i = 0; i<this.lista.length; i++) {
+                if (comparador.equals(this.lista[i].trim().toLowerCase())) {
+                    ultimoIndice = i;
+                }
+            }
+            return ultimoIndice;
+        }
+    }
+
+    //Júlio
+    @Override
+    public int contarOcorrencias(String elemento){
+        int ocorrencias = 0;
+        for(int i = 0; i < this.lista.length; i++){
+            if(this.lista[i] != null && this.lista[i].equals(elemento)){
+                ocorrencias++;
+            }
+        }
+        System.out.println("O elemento " + elemento + " foi encontrado " + ocorrencias + " vezes na lista.");
+        return ocorrencias;
+    }
+    //Eduardo
+    @Override
+    public int substituir(String antigo, String novo) {
+        return 0;
+    }
+}
